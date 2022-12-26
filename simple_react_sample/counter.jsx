8f2b17bebf8a2dc9817cb5c078a67ca61126a46f
H.K <16552133+bj1024@users.noreply.github.com>
2022-12-26 23:42:25 +0900

    wrap for debug.

--- simple_react_sample/counter.jsx
+++ simple_react_sample/counter.jsx
@@ -2,17 +2,22 @@ const { useEffect, useState } = React
 
 function Counter() {
 
-  const [count, setCount] = useState(99990);
 
+  const [count, setCount]
+    = useState(99990);
+
+  useEffect(() => {
+    console.log("useEffect")
+  }, [])
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button 
-      onClick={() => {
-        console.log("button onclick")
-        setCount(count + 1)
-      }
-      }>
+      <button
+        onClick={() => {
+          console.log("button onclick")
+          setCount(count + 1)
+        }
+        }>
         Click me
       </button>
     </div>
